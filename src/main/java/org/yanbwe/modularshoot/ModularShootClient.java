ec467b95888754a98412b202d25f25491999f64a
Yanbwe <GGxiaoyang1@163.com>
2026-06-19 22:19:10 +0800

    refactor(client): 移除客户端侧的事件总线订阅注解
    同时添加物品和属性的英文本地化文本

--- src/main/java/org/yanbwe/modularshoot/ModularShootClient.java
+++ src/main/java/org/yanbwe/modularshoot/ModularShootClient.java
@@ -2,13 +2,11 @@ package org.yanbwe.modularshoot;
 
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(value = ModularShoot.MODID, dist = Dist.CLIENT)
-@EventBusSubscriber(modid = ModularShoot.MODID, value = Dist.CLIENT)
 public class ModularShootClient {
     public ModularShootClient(ModContainer container) {
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
